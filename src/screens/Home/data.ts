a7964cc00775d862149b6cb90f63c61f88cb6ef5
AsadbekNurmamtov2002 <nurmamtovasadbek85@gmail.com>
2024-05-14 09:58:56 +0500

    m3

---
+++ src/screens/Home/data.ts
@@ -0,0 +1,6 @@
+import { FlagTest } from "../../data";
+import { HayuqTest } from "../../data/hayuq";
+import { SolarSystemTest } from "../../data/solarSystem";
+import { Test } from "../../data/types";
+
+export const data: Test[] = [FlagTest, SolarSystemTest, HayuqTest];
